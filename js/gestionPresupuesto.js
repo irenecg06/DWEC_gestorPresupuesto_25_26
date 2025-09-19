a3801da93c89d6d40986b06e84815fa6746a1e0d
irenecg06 <irenecabreragomez06@gmail.com>
2025-09-19 19:50:31 +0200

    gestionPresupuesto.js practica terminada 19/09

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,48 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(nuevoPresupuesto) {
     // TODO
+    if (isNaN(nuevoPresupuesto) || nuevoPresupuesto < 0.0)
+    {
+        console.log('El presupuesto introducido no es válido.')
+        return -1;
+    }
+    else {
+        presupuesto = nuevoPresupuesto;
+    }
+    return presupuesto;
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return 'Tu presupuesto actual es de ' + presupuesto + ' €'
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    if (isNaN(valor) || valor < 0.0){
+        valor = 0;
+    }
+        this.descripcion = descripcion;
+        this.valor = valor;
+
+        this.mostrarGasto = function() {
+            return 'Gasto correspondiente a ' + this.descripcion + ' con valor ' + this.valor + ' €'
+        };
+
+        this.actualizarDescripcion = function(nuevaDescripcion) {
+            this.descripcion = nuevaDescripcion;
+        };
+
+        this.actualizarValor = function(nuevoValor) {
+            if(nuevoValor >= 0.0)
+                this.valor = nuevoValor;
+        };
+    
+    return this;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
