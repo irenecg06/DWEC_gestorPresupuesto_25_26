e7bce99fb2869b666f4d3f52e35eac1fd2bff9b6
irenecg06 <irenecabreragomez06@gmail.com>
2025-12-12 18:59:56 +0100

    contenido agregado a mi practica expresiones regulares

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -165,6 +165,10 @@ function agruparGastos(periodo = "mes", etiquetas = [], fechaDesde, fechaHasta =
     return gastosReduce;
 }
 
+function transformarListadoEtiquetas(){
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -178,5 +182,6 @@ export   {
     calcularTotalGastos,
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
